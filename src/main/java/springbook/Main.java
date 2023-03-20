338d7df1e1317a4a1415efc993fc00b4ef686b7e
Hyeonjun Park <phjppo0918@gmail.com>
2023-03-20 22:33:53 +0900

    1.3.3 : 관계설정 책임의 분리

--- src/main/java/springbook/Main.java
+++ src/main/java/springbook/Main.java
@@ -1,5 +1,6 @@
 package springbook;
 
+import springbook.user.dao.DConnectionMaker;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -7,7 +8,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
+        UserDao dao = new UserDao(new DConnectionMaker());
         User user = new User();
         user.setId("whiteship2");
         user.setName("백기선");
