30280230ddc2022c9ec778511943a299e5962f36
Hyeonjun Park <phjppo0918@gmail.com>
2023-03-23 14:35:57 +0900

    3.6.F : jdbcTemplate 적용

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -10,35 +10,33 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.dataSource = dataSource;
     }
+
+    private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<User> userMapper =
+            new RowMapper<User>() {
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+            };
+
 
     public void add(final User user) {
         this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
                 user.getId(), user.getName(), user.getPassword());
     }
 
-
     public User get(String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[] {id},
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum)
-                            throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                });
+                new Object[] {id}, this.userMapper);
     }
-
 
     public void deleteAll() {
         this.jdbcTemplate.update("delete from users");
@@ -49,17 +47,7 @@ public class UserDao {
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",
-                new RowMapper<User>() {
-                    public User mapRow(ResultSet rs, int rowNum)
-                            throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
-                        return user;
-                    }
-                });
+        return this.jdbcTemplate.query("select * from users order by id",this.userMapper);
     }
 
 }
